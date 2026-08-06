71c4a47156b897c1f19b4f783000aaa7a9b233e3
Israel <Is0752206@gmail.com>
2026-08-05 21:09:24 -0300

    Projeto de integração com a API Fiscal Data

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
-# Integracoes_apis_terceiros
+Projeto de Integrações_apis_terceiros
 
+O projeto realiza uma integração com a API Fiscal data.
+* Resumo
+  - Realizando uma requisição HTTP GET.
+  - Verificação do Status Code da resposta.
+  - Percorrer ao possíveis falhas de conexão.
+  - Converte a resposta para JSON.
+  - Esse irá exiber os cincos primeiros registros retornados pela API.
 
-<!-- Tem a finalidade de organizar integrações com o sistema web ERP conta azul.  -->
+* Ferramentas ulilizadas
+  - Python (Principal ferramenta para criar o programa)
+  - Requests (A nossa requisição para buscar a URL)
+  - API (Sistema intermediário para troca entre  dados em sistemas )
+  - JSON (Converte ás taxas e data em JSON, formato padrão para troca de sistema)
 
-<!-- https://developers.contaazul.com/aboutapis -->
+*Estudos
+ - Com esse projeto tenho a noção de que é mexer com API, sistema que permitir trocar de dados entre sistemas, podendo aprender ainda mais com praticas e com novos desafios.
+
+*Funções e seus objetivos
+ -Import Requests ( Usei a biblioteca "Requests", para pode usar ás ferramentas certas para poder criar o programa)
+ - Definir a URL da API (Como é Integrações_apis_terceiros, resolvi pegar a da fiscal data - https://api.fiscaldata_treasury... )
+ - Depois usamos a requisição para http GET.
+ - Usamos try,except (Possíveis erros) com if,elif,else (Caminhos provaveis) para que o comando não tenha erros de trava ou caminhos sem rumo
+ - Usando For para percorrer os registros e depois exibir os cincos primeiros resgistros
+ - E por fim mostrar o resultado na tela 
--- index.py
+++ index.py
@@ -1,1 +1,37 @@
+import requests
 
+def buscar_taxas():
+
+    url = "https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v1/accounting/od/rates_of_exchange"
+
+    try:
+        resposta = requests.get(url)
+        if resposta.status_code == 200:
+            print("Registros encontrados com sucesso!")
+
+            dados = resposta.json()
+            for registro in dados["data"][:5]:
+                    print("Data:", registro["record_date"])
+                    print("Taxa de câmbio:", registro["exchange_rate"])
+                    print("---------------------------")
+        elif resposta.status_code == 400:
+             print("Requisição inválida. Verifique os parâmetros da solicitação.")
+        elif resposta.status_code == 401:
+                    print("Acesso não autorizado.")
+        elif resposta.status_code == 403:
+            print("Acesso proibido. Você não tem permissão para acessar este recurso.")
+        elif resposta.status_code == 404:
+                    print("Recurso não encontrado.")
+        elif resposta.status_code == 500:
+                    print("Erro interno do servidor. Tente novamente mais tarde.")
+        elif resposta.status_code == 503:
+                    print("Serviço indisponível. O servidor está temporariamente indisponível.")
+        elif resposta.status_code == 429:
+                    print("Limite de taxa excedido. Aguarde antes de fazer novas solicitações.")
+        else:
+            print(f"Erro: {resposta.status_code}")
+
+    except requests.RequestException as erro:
+        print(f"Erro de conexão: {erro}")
+
+buscar_taxas()
